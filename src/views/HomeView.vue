1db9ac9ea84941e90d3d35857477972fd322e10c
Name <anushqakandan03@gmail.com>
2024-07-01 09:41:52 +0200

    Home Layout

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+    <section class="row" id="home">
+        <LandingSection />
+    </section>
+  
+    <section class="row" id="about">
+       <AboutSection />
+    </section>
+    
+    <section class="row" id="resume">
+       <ResumeSection />
+    </section>
+    
+    <section class="row" id="project"></section>
+    
+    <section class="row" id="testimonials"></section>
+    
+    <section class="row" id="reachMe"></section>
+    
+    </div>
+  </template>
+  
+  <script>
+  import LandingSection from '@/components/LandingSection.vue'
+  import AboutSection from '@/components/AboutSection.vue'
+  import ResumeSection from '@/components/ResumeSection.vue'
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      LandingSection,
+     AboutSection,
+     ResumeSection
+     
+    }
+  }
+  </script>
+  
