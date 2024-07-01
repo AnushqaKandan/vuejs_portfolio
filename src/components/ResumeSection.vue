15ca8265d454f93077fddf8c1019e50212363b08
Name <anushqakandan03@gmail.com>
2024-07-01 12:45:22 +0200

    Stling Added

--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -9,6 +9,7 @@
         <div class="card text-white">
           <div id="details">
               <div v-if="resume?.length">
+                <img src="https://anushqakandan.github.io/All-images/vue/SpineRoad2.png " alt="resume" class="img-resume1" loading="lazy">
                   <p class="Resume-Title">{{ resume[0].place }}</p>
                 <p class="Resume-Description">{{ resume[0].description }}</p>
               </div>
@@ -18,6 +19,7 @@
         <div class="card text-white">
           <div id="details">
               <div v-if="resume?.length">
+                <img src="https://anushqakandan.github.io/All-images/vue/UWC1.png " alt="resume" class="img-resume2" loading="lazy">
                   <p class="Resume-Title">{{ resume[1].place }}</p>
                 <p class="Resume-Description">{{ resume[1].description }}</p>
               </div>
@@ -28,6 +30,7 @@
         <div class="card text-white">
           <div id="details">
               <div v-if="resume?.length">
+                <img src="https://anushqakandan.github.io/All-images/vue/Sigma2.png " alt="resume" class="img-resume3" loading="lazy">
                   <p class="Resume-Title">{{ resume[2].place }}</p>
                 <p class="Resume-Description">{{ resume[2].description }}</p>
               </div>
@@ -38,6 +41,7 @@
         <div class="card text-white">
           <div id="details">
               <div v-if="resume?.length">
+                <img src="https://anushqakandan.github.io/All-images/vue/LifeChoices.png " alt="resume" class="img-resume4" loading="lazy">
                   <p class="Resume-Title">{{ resume[3].place }}</p>
                 <p class="Resume-Description">{{ resume[3].description }}</p>
               </div>
@@ -62,6 +66,3 @@
   
   </script>
   
-  <style>
-  
-  </style>
